9a296a855853fa27dcd4d7abc7f302b17ad5d1ac
THACHER <sophie.thacher@gmail.com>
2016-02-23 16:46:53 -0800

    it works!  plus cosmetic fixes!

---
+++ script.js
@@ -0,0 +1,64 @@
+var body = document.getElementsByTagName('body')[0];
+var wrapper = document.createElement('section');
+body.appendChild(wrapper);
+// wrapper.style.width = '560px';
+// wrapper.style.width = '450px';
+
+for(var i = 0; i < 4; i++){
+  for(var j = 0; j < 16; j++){
+    var squares = document.createElement('div');
+    squares.style.height = '11.1%';
+    squares.style.width = '11.1%';
+    // squares.style.float = 'left';
+    squares.style.paddingTop = '10%';
+    squares.style.margin = '0% 0.5% 0px 0px';
+    squares.style.display = 'inline-block';
+    squares.style.border = '1px solid black';
+document.getElementsByTagName('section')[0].appendChild(squares);
+ }
+}
+
+var palette = document.createElement('section');
+body.appendChild(palette);
+
+var colors = ['red', 'blue', 'purple','green','yellow','pink','orange','grey']
+
+for(var i = 0; i < 1; i++){
+  for(var j = 0; j < 8; j++){
+    var paletteSquares = document.createElement('div');
+    paletteSquares.style.height = '11.1%';
+    paletteSquares.style.width = '11.1%';
+    // squares.style.float = 'left';
+    paletteSquares.style.paddingTop = '10%';
+    paletteSquares.style.margin = '2% 0.5% 0px 0px';
+    paletteSquares.style.display = 'inline-block';
+    paletteSquares.style.border = '1px solid black';
+    paletteSquares.style.backgroundColor = colors[j];
+    paletteSquares.addEventListener('click',targetingColor);
+    document.getElementsByTagName('section')[1].appendChild(paletteSquares);
+ }
+}
+
+var paletteWrapper = document.getElementsByTagName('section')[1];
+
+
+//PaletteColors
+// var lastSquare = document.getElementsByTagName('div');
+// lastSquare[lastSquare.length-1].style.backgroundColor = 'blue';
+// lastSquare[lastSquare.length-2].style.backgroundColor = 'purple';
+// lastSquare[lastSquare.length-3].style.backgroundColor = 'red';
+
+//Do Stuff
+
+function targetingColor(someEvent) {
+  saveColor = someEvent.target.style.backgroundColor;
+}
+
+
+var clickSquares = document.querySelector('section');
+var saveColor = 'red';
+function targeting (event) {
+  event.target.style.background = saveColor;
+  // console.log(event.target.style.background);
+}
+clickSquares.addEventListener('click',targeting);
