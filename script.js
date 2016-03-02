99bdd947e3701fd6194da97d13664d1204a482b0
THACHER <sophie.thacher@gmail.com>
2016-03-01 18:33:34 -0800

    feedback updates - Mar 1 resubmission

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ for(var i = 0; i < 8; i++){
   row.style.clear = 'both';
   wrapper.appendChild(row);
   for(var j = 0; j < 8; j++){
-    var squares = document.createElement('div');
-    squares.style.height = heightWidth;
-    squares.style.width = heightWidth;
-    squares.style.float = 'left';
-    squares.style.paddingTop = '10%';
-    squares.style.margin = '0.5% 0.5% 0px 0px';
-    // squares.style.display = 'inline-block';
-    squares.style.border = '1px solid black';
-    squares.style.boxSizing = 'border-box';
-    row.appendChild(squares);
-// document.getElementsByTagName('section')[0].appendChild(squares);
- }
+    var square = document.createElement('div');
+    square.style.height = heightWidth;
+    square.style.width = heightWidth;
+    square.style.float = 'left';
+    square.style.paddingTop = '10%';
+    square.style.margin = '0.5% 0.5% 0px 0px';
+    // square.style.display = 'inline-block';
+    square.style.border = '1px solid black';
+    square.style.boxSizing = 'border-box';
+    row.appendChild(square);
+    // document.getElementsByTagName('section')[0].appendChild(square);
+  }
 }
 
 var palette = document.createElement('section');
@@ -30,27 +30,30 @@ var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple','pink','grey'
 
 for(var i = 0; i < 1; i++){
   for(var j = 0; j < 8; j++){
-    var paletteSquares = document.createElement('div');
-    paletteSquares.style.height = heightWidth;
-    paletteSquares.style.width = heightWidth;
-    // squares.style.float = 'left';
-    paletteSquares.style.paddingTop = '10%';
-    paletteSquares.style.margin = '2% 0.5% 0px 0px';
-    paletteSquares.style.display = 'inline-block';
-    paletteSquares.style.border = '1px solid black';
-    paletteSquares.style.backgroundColor = colors[j];
-    paletteSquares.addEventListener('click',targetingColor);
-    document.getElementsByTagName('section')[1].appendChild(paletteSquares);
- }
+    var paletteSquare = document.createElement('div');
+    paletteSquare.style.height = heightWidth;
+    paletteSquare.style.width = heightWidth;
+    // square.style.float = 'left';
+    paletteSquare.style.paddingTop = '10%';
+    paletteSquare.style.margin = '2% 0.5% 0px 0px';
+    paletteSquare.style.display = 'inline-block';
+    paletteSquare.style.border = '1px solid black';
+    paletteSquare.style.backgroundColor = colors[j];
+    paletteSquare.addEventListener('click',targetingColor);
+    document.getElementsByTagName('section')[1].appendChild(paletteSquare);
+  }
 }
 var paletteWrapper = document.getElementsByTagName('section')[1];
 
 function targetingColor(someEvent) {
   saveColor = someEvent.target.style.backgroundColor;
 }
-var clickSquares = document.querySelector('section');
+var clickSquare = document.querySelector('section');
 var saveColor = 'red';
 function targeting (event) {
-  event.target.style.background = saveColor;
+    // console.log(event.target.childElementCount);
+    if(event.target.childElementCount === 0){
+    event.target.style.background = saveColor;
+  }
 }
-clickSquares.addEventListener('click',targeting);
+clickSquare.addEventListener('click',targeting);
